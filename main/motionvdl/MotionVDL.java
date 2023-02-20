22ee822c4abfbc548aabfab4ca789e54812ab273
Joseph Perry <joseph.perry@tutanota.com>
2023-02-20 21:52:54 +0000

    Refactored test classes

--- main/motionvdl/MotionVDL.java
+++ main/motionvdl/MotionVDL.java
@@ -5,16 +5,24 @@ import motionvdl.display.Display;
 import motionvdl.model.Video;
 
 /**
- * MotionVDL application starter
+ * MotionVDL application starter 
  * @author Joseph
  */
 public class MotionVDL {
 	
 	/**
 	 * MotionVDL starter program
-	 * @param args Location of the video on disk
+	 * defines the program arguments
+	 * @param args[0] Video on filesystem
+	 * @param args[1] Setup-debug flag
 	 */
 	public static void main(String[] args) {
+		
+		// throw insufficient arguments
+		if (args.length == 0) throw new IllegalArgumentException("Insufficient program arguments");
+		
+		// setup debug
+		if (args.length >= 2) Debug.setup(Boolean.parseBoolean(args[1]));
 		
 		// start program with video file
 		new MainController(new Display()).start(Video.fromFile(args[0]));
--- test/motionvdl/TestMVDL.java
+++ test/motionvdl/TestMVDL.java
@@ -8,16 +8,71 @@ import motionvdl.controller.ColorController;
 import motionvdl.controller.Controller;
 import motionvdl.controller.CropController;
 import motionvdl.controller.LabelController;
+import motionvdl.controller.MainController;
 import motionvdl.controller.ScaleController;
 import motionvdl.display.Display;
 import motionvdl.model.Video;
 import motionvdl.util.Numbers;
 
-class UnitTestMVDL extends Controller {
+public class TestMVDL {
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		Debug.setup(true);
+	}
+	
+	
+	/**
+	 * Example of start to finish instruction 
+	 * sequence for a fully integrated system
+	 */
+	@Test
+	public void fullProgram() {
+		
+		// setup components
+		Video noise = Video.noise(250,200,50);
+		Display display = new Display();
+		MainController controller = new MainController(display);
+		
+		// start the main controller
+		controller.start(noise);
+		
+		// crop stage
+		controller.point(Numbers.biasRand(49,100), Numbers.biasRand(49,100)); // place first point
+		controller.point(Numbers.biasRand(49,100), Numbers.biasRand(49,100)); // place second point
+		controller.point(Numbers.biasRand(49,100), Numbers.biasRand(49,100)); // clear points
+		controller.point(Numbers.biasRand(49,100), Numbers.biasRand(49,100)); // place first point
+		controller.point(Numbers.biasRand(49,100), Numbers.biasRand(49,100)); // place second point
+		controller.process(); // commit crop
+		controller.complete(); // next stage
+		
+		// scale stage
+		controller.process(); // scale to target resolution
+		controller.complete(); // next stage
+		
+		// color stage
+		controller.process(); // convert to greyscale
+		controller.process(); // back to color
+		controller.process(); // back to greyscale
+		controller.complete(); // next stage
+		
+		// labelling stage
+		controller.process(); // try to undo a point on the first frame when it's empty
+		for (int i=0; i < 50; i++) {
+			for (int j=0; j < 11; j++) {
+				controller.point(Numbers.biasRand(49,100), Numbers.biasRand(49,100)); // on each frame place 11 points
+			}
+		}
+		controller.process(); // undo the last point
+		controller.complete(); // try to export files, should be blocked because of the incomplete label
+		controller.point(Numbers.biasRand(49,100), Numbers.biasRand(49,100)); // place a point
+		controller.point(Numbers.biasRand(49,100), Numbers.biasRand(49,100)); // try placing a 12th point on the last frame
+		try {
+			controller.complete(); // try to export files, expect unsupported exception until implemented
+			Assertions.fail(); // fail if there is no exception from controller
+		} catch (UnsupportedOperationException e) {
+			Debug.trace(e.getMessage());
+		}
 	}
 	
 	
