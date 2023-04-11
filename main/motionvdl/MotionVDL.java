2902cb65977292eb526edff67d3761bd4cb9870a
Joseph Perry <joseph.perry@tutanota.com>
2023-04-11 11:21:26 +0100

    Fixed starter method

--- main/motionvdl/MotionVDL.java
+++ main/motionvdl/MotionVDL.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import motionvdl.controller.MainController;
 import motionvdl.display.Display;
-import motionvdl.model.Video;
 
 import java.util.List;
 
@@ -25,25 +24,23 @@ public class MotionVDL extends Application {
 
 	@Override
 	public void start(Stage stage) {
-
+		
 		// need to use Parameters to get args[], and then save in List
 		Parameters params = getParameters();
 		List<String> args = params.getRaw();
-
+		
 		// throw insufficient arguments
 		if (args.size() == 0) throw new IllegalArgumentException("Insufficient arguments");
-
+		
 		// setup debug
 		if (args.size() >= 2) Debug.setup(Boolean.parseBoolean(args.get(1)));
-
+		
 		// setup display and controller
 		Display display = new Display(675, 475, stage);
 		MainController controller = new MainController(display);
-		display.sendTo(controller);
-
-		// start main controller with video file
-		//controller.pass(Video.fromFile(args.get(0)));
-		controller.pass(Video.noise(1920, 1080, 5));
+		
+		// start main controller with mvdl file
+		controller.open("output.mvdl");
 		display.setViewPort();
 	}
 }
