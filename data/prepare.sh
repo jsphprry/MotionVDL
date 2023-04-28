12a5a6c8dcb94bd52e643eb3fcf9c100b3ab2de1
Joseph Perry <joseph.perry@tutanota.com>
2023-04-28 13:09:31 +0100

    Reformatted preprocessing script

--- data/prepare.sh
+++ data/prepare.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-rm frames/* -v
-ffmpeg -i $1 -vf scale=722:-1 -an downscale.mp4
-ffmpeg -i downscale.mp4 -vf fps=10 "$(pwd)/frames/%d.png"
 
+# The frames directory should contain only the processed video frames as pngs, 
+# any non-image files in this directory will cause an exception in the file loader
+
+# prepare directory
+mkdir -pv frames/
+rm -rfv frames/*
+
+# sample images at 10fps from downscaled input video '$1'
+ffmpeg -i $1 -vf scale=722:-1 -an downscale.mp4
+ffmpeg -i downscale.mp4 -vf fps=10 "frames/%d.png"
+
+# remove file extensions (unused)
 #cd frames/
 #for file in *; do
 #	filename=$(basename "$file" .png)
