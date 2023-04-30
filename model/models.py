0e4d25fd71f4693a5ee4789cace38037a962fe18
Joseph Perry <joseph.perry@tutanota.com>
2023-04-30 22:22:37 +0100

    Updates to model tests and figures

--- model/models.py
+++ model/models.py
@@ -4,8 +4,48 @@ from tensorflow.keras import layers
 
 
 
-# cnn+lstm sequential model
-class CNNLSTM(Sequential):
+# dense 
+class Dense(Sequential):
+	def __init__(self):
+		super().__init__()
+		self.add(layers.Reshape((1,50*50), input_shape=(50,50)))
+		self.add(layers.Dense(22, activation='sigmoid'))
+		self.add(layers.Reshape((11,2)))
+		self._name = "Dense"
+
+
+
+# convolutional
+class Conv(Sequential):
+	def __init__(self):
+		super().__init__()
+		self.add(layers.Reshape((50,50,1), input_shape=(50,50)))	# 1,50,50
+		self.add(layers.Conv2D(3, 9, activation='sigmoid'))			# 3,42,42
+		self.add(layers.MaxPooling2D())								# 3,21,21
+		self.add(layers.Conv2D(3, 5, activation='sigmoid'))			# 3,17,17
+		self.add(layers.MaxPooling2D())								# 3,8,8
+		self.add(layers.Conv2D(3, 5, activation='sigmoid'))			# 3,13,13
+		self.add(layers.MaxPooling2D())								# 3,6,6
+		self.add(layers.Reshape((1,12)))
+		self.add(layers.Dense(22))
+		self.add(layers.Reshape((11,2)))
+		self._name = "Conv"
+
+
+
+# dense 
+class LSTM(Sequential):
+	def __init__(self):
+		super().__init__()
+		self.add(layers.Reshape((1,50*50), input_shape=(50,50)))
+		self.add(layers.LSTM(22, activation='sigmoid'))
+		self.add(layers.Reshape((11,2)))
+		self._name = "LSTM"
+
+
+
+# convolutional+lstm
+class CnnLSTM(Sequential):
 	def __init__(self):
 		super().__init__()
 		self.add(layers.Reshape((50,50,1), input_shape=(50,50)))
@@ -14,40 +54,18 @@ class CNNLSTM(Sequential):
 		self.add(layers.Reshape((1,3*8*8)))
 		self.add(layers.LSTM(22))
 		self.add(layers.Reshape((11,2)))
+		self._name = "CnnLSTM"
 
 
 
-# convolutional lstm sequential model
+# convolutional lstm
 class ConvLSTM(Sequential):
 	def __init__(self):
 		super().__init__()
 		self.add(layers.Reshape((1,50,50,1), input_shape=(50,50)))
-		self.add(layers.ConvLSTM2D(3, 3, strides=3))
+		self.add(layers.ConvLSTM2D(5, 9))
 		self.add(layers.MaxPooling2D())
-		self.add(layers.Reshape((1,1*3*8*8)))
+		self.add(layers.Reshape((1,1*5*21*21)))
 		self.add(layers.Dense(22, activation='sigmoid'))
 		self.add(layers.Reshape((11,2)))
-
-
-
-# cnn sequential model
-class CNN(Sequential):
-	def __init__(self):
-		super().__init__()
-		self.add(layers.Reshape((50,50,1), input_shape=(50,50)))
-		self.add(layers.Conv2D(3, 3, strides=3, activation='sigmoid'))
-		self.add(layers.MaxPooling2D())
-		self.add(layers.Reshape((1,3*8*8)))
-		self.add(layers.Dense(22, activation='sigmoid'))
-		self.add(layers.Reshape((11,2)))
-
-
-
-# dense sequential model
-class Dense(Sequential):
-	def __init__(self):
-		super().__init__()
-		self.add(layers.Reshape((1,50*50), input_shape=(50,50)))
-		self.add(layers.Dense(25, activation='sigmoid'))
-		self.add(layers.Dense(22, activation='sigmoid'))
-		self.add(layers.Reshape((11,2)))
+		self._name = "ConvLSTM"
--- model/test_models.py
+++ model/test_models.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 
-# get gtk warnings out the way
+# print gtk warnings
 plt.plot()
 
 # load and partition data
@@ -17,28 +17,34 @@ test_x = x[430:800]
 test_y = y[430:800]
 
 # initialise models
-_models = [] # list of tuples (model, model-name)
-_models.append((models.CNNLSTM(),'CNNLSTM'))
-_models.append((models.ConvLSTM(),'ConvLSTM'))
-_models.append((models.CNN(),'CNN'))
-_models.append((models.Dense(),'Dense'))
+_models = []
+_models.append(models.Dense())
+_models.append(models.Conv())
+_models.append(models.LSTM())
+_models.append(models.CnnLSTM())
+_models.append(models.ConvLSTM())
+
+# print summaries
+for model in _models:
+	model.summary()
 
 # test models
-for model,name in _models:
+for model in _models:
 	
 	# compile model
 	model.compile(loss='mean_squared_error', optimizer='adam')
-	model.summary()
 	
 	# fit model and record history
-	history = model.fit(train_x, train_y, epochs=100, batch_size=1, verbose=2)
-	plt.plot(history.history['loss'], label=name)
+	print()
+	print(model._name)
+	history = model.fit(train_x, train_y, epochs=10, batch_size=1, verbose=2)
+	plt.plot(history.history['loss'], label=model._name)
 	
 	# output predictions
 	train_predict = model.predict(train_x)
 	test_predict = model.predict(test_x)
-	utility.plot_export(train_x, train_predict, filename=name, figtitle=f"{name} train")
-	utility.plot_export(test_x, test_predict, start=len(train_x), filename=name, figtitle=f"{name} test")
+	utility.plot_export(train_x, train_predict, filename=model._name, figtitle=f"{model._name} train")
+	utility.plot_export(test_x, test_predict, start=len(train_x), filename=model._name, figtitle=f"{model._name} test")
 
 # plot histories
 plt.xlabel('epoch')
